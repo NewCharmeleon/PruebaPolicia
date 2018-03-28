e759e7844ce151f46b0ec4b2067a75a35fae4569
NewCharmeleon <Dongato.tlw@gmail.com>
2018-03-28 09:52:55 -0300

    Query Medias y function resize js

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -1,24 +1,4 @@
-/*$(document).ready(function(){
-	var altura = $('.navbar-fixed-top').offset().top;
-	$(window).on('scroll', function(){
-		if ( $(window).scrollTop() > altura ){
-			$('.navbar-fixed-top').addClass('navbar-sticky');
-		} else {
-			$('.navbar-fixed-top').removeClass('navbar-sticky');
-		}
-	});
-});*/
-/*
-$(document).ready(function(){
-	var altura = $('.navbar-fixed-top').offset().top;
-	$(window).on('scroll', function(){
-		if ( $(window).scrollTop() > altura ){
-			document.'navbar-fixed-top.style.backgroundColor'.setAttribute = 'transparent';
-		} else {
-			document.'navbar-fixed-top.style.backgroundColor' = '#81BEF7';
-		}
-	});
-});*/
+
 $(document).ready(function(){
 	var altura = $('.navbar-fixed-top').offset().top;
 	var escudoLeftW = $(".escudoLeft").width();
@@ -37,21 +17,93 @@ $(document).ready(function(){
 			$(".escudoRight").width(escudoLeftW+"px");
 		}
 	});
-});
-$(document).ready(function(){
 	$(".collapse").on('show.bs.collapse', function(){
 		$(".escudoRight").hide();
 	});
-});
-$(document).ready(function(){
 	$(".collapse").on('hide.bs.collapse', function(){
 		$(".escudoRight").show();
 	});
-});
-$(document).ready(function(){
-	var hgroupS = $(".video").width();
-	alert("el video cambio");
-	$(".video").resize(function(){
-		$(".hgroup").width(hgroupS+"rem"); 
 
-}
+	$(window).resize(
+
+		function () {    
+    		var width = document.documentElement.clientWidth;
+    
+   			 if (width < 1500 && width >= 900){
+     			 document.getElementById('textoVideo').style.fontSize = '10rem';
+     			 document.getElementById('lema').style.fontSize = '4rem';
+      
+   			 }else if (width < 900 && width >= 720){
+     			 document.getElementById('textoVideo').style.fontSize = '8rem';
+     			 document.getElementById('lema').style.fontSize = '3rem';
+      			  $('.btn').addClass('btn-lg');
+		    }else if(width < 720 && width >= 580){
+     				document.getElementById('textoVideo').style.fontSize = '6rem';
+      				document.getElementById('lema').style.fontSize = '2rem';
+      				$('.btn.btn-lg').removeClass('btn-lg');
+      				$('.btn').removeClass('btn-sm');
+	  
+    		}
+    		else if(width < 580 && width >= 440){
+     				 document.getElementById("textoVideo").style.fontSize = "4rem";
+      				 document.getElementById('lema').style.fontSize = '1.5rem';
+      				 $('.btn.btn-xs').removeClass('btn-xs');
+       				$('.btn').addClass('btn-sm');
+    		}
+    		else if(width<440 && width>=0){
+    				document.getElementById("textoVideo").style.fontSize = "3rem";
+      				 document.getElementById('lema').style.fontSize = '1rem';
+    			  $('.btn.btn-sm').removeClass('btn-sm');
+     			 $('.btn').addClass('btn-xs');
+   			 }
+   			 else{
+     			 document.getElementById("textoVideo").style.fontSize = "10rem";
+     		  document.getElementById('lema').style.fontSize = '4rem';
+
+
+    }
+});
+
+});
+
+/*window.addEventListener("resize", textoVideo);*/
+/*$(window).resize(
+function () {    
+    var width = document.documentElement.clientWidth;
+    
+    if (width < 1500 && width >= 900){
+      document.getElementById('textoVideo').style.fontSize = '10rem';
+      document.getElementById('lema').style.fontSize = '4rem';
+      
+    }if (width < 900 && width >= 720){
+      document.getElementById('textoVideo').style.fontSize = '8rem';
+      document.getElementById('lema').style.fontSize = '3rem';
+      
+	  $('.btn').addClass('btn-lg');
+
+    }else if(width < 719 && width >= 580){
+     document.getElementById('textoVideo').style.fontSize = '6rem';
+      document.getElementById('lema').style.fontSize = '2rem';
+      $('.btn.btn-lg').removeClass('btn-lg');
+      $('.btn').removeClass('btn-sm');
+	  
+    }
+    else if(width < 580 && width >= 440){
+      document.getElementById("textoVideo").style.fontSize = "4rem";
+       document.getElementById('lema').style.fontSize = '1.5rem';
+       $('.btn.btn-xs').removeClass('btn-xs');
+       $('.btn').addClass('btn-sm');
+    }
+    else if(width<440 && width>=0){
+    	document.getElementById("textoVideo").style.fontSize = "3rem";
+       document.getElementById('lema').style.fontSize = '1rem';
+      $('.btn.btn-sm').removeClass('btn-sm');
+      $('.btn').addClass('btn-xs');
+    }
+    	else{
+      document.getElementById("textoVideo").style.fontSize = "10rem";
+       document.getElementById('lema').style.fontSize = '4rem';
+
+
+    }
+});*/
