bb0b5c5596495644303ff67bbb57aa36d209f60b
NewCharmeleon <Dongato.tlw@gmail.com>
2018-04-05 08:47:49 -0300

    migraciones y modelo creado

--- apps/publico/models.py
+++ apps/publico/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Publicacion(models.Model):
+	TIPO = ((1 , 'articulo'), (2, 'video'),)
+
+	tipo = models.IntegerField(choices = TIPO)
+	descripcion = models.CharField(max_length=100)
+	titulo = models.CharField(max_length=50)
+	contenido = models.TextField()
+	url = models.URLField()
+	fecha_creacion = models.DateField()
+	fuente = models.CharField(max_length=100)
+	usuario = models.ForeignKey(User, on_delete = 'CASCADE')
