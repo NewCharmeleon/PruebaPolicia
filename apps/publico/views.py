a3faaa6257d79ec01f3382ac8b404110117e888e
NewCharmeleon <Dongato.tlw@gmail.com>
2018-07-31 09:32:38 -0300

    Cambios en Noticias y link Ultimas Noticias del Index

--- apps/publico/views.py
+++ apps/publico/views.py
@@ -10,16 +10,12 @@ from django.urls import reverse
 
 def home(request):
 
-	#noticias = Publicacion.objects.filter(is_para_portada=True, tipo=1)
+	#noticia=get_object_or_404(Publicacion, id=id)
 	noticias = Publicacion.objects.filter(is_para_portada=True, is_publicado=True, tipo=1).order_by('-fecha_creacion')[:10]
 	videos = Publicacion.objects.filter(is_para_portada=True, tipo=2)
 	autoridad = Autoridad.objects.filter(is_para_portada=True, cargo_id__in=[1,2])
-	#print ("hay")
-	#print(len(autoridad))
-	#argo = Autoridad.CARGO
-	#jerarquia = Autoridad.JERARQUIA
-	#dependencia = Autoridad.DEPENDENCIA
 
+	
 
 	return render(request,"indexMBR.html",{'nombre':"Adrian", 'noticias' :noticias, 'videos':videos, 'autoridades':autoridad })
 def noticias(request):
