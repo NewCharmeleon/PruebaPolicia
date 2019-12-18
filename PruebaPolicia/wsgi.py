d777f682f00fda87327af9122cc8f13093ee1c38
NewCharmeleon <Dongato.tlw@gmail.com>
2019-12-18 10:28:26 -0300

    ReAcomodamientos del Proyecto

--- PruebaPolicia/wsgi.py
+++ PruebaPolicia/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PruebaPolicia.settings")
-sys.path.append('/var/django-projects/PruebaPolicia/')
-sys.path.append('/var/django-projects')
 
 
 application = get_wsgi_application()
